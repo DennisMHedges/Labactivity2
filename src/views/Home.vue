e253061c4cd9303570078ae2783e6e4251ad5a6f
DennisMHedges <62683470+DennisMHedges@users.noreply.github.com>
2020-03-26 12:48:58 +0900

    Add files via upload
    lab activity 2

---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+<div>>
+    <header class="py-5 bg-image-full" style="background-image: url('https://unsplash.it/1900/1080?image=1076');">
+    <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200&text=Logo" alt="">
+  </header>
+
+  <!-- Content section -->
+  <section class="py-5">
+    <div class="container">
+      <h1>Coronavirus: What is a pandemic and why use the term now?</h1>
+      <p class="lead">A pandemic describes an infectious disease where we see significant and ongoing person-to-person spread in multiple countries around the world at the same time.
+
+The last time a pandemic occurred was in 2009 with swine flu, which experts think killed hundreds of thousands of people.
+
+Pandemics are more likely if a virus is brand new, able to infect people easily and can spread from person-to-person in an efficient and sustained way.
+
+Coronavirus appears to tick all of those boxes.
+
+With no vaccine or treatment that can prevent it yet, containing its spread is vital.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
+    </div>
+  </section>
+
+  <!-- Image Section - set the background image for the header in the line below -->
+  <section class="py-5 bg-image-full" style="background-image: url('https://unsplash.it/1900/1080?image=1081');">
+    <!-- Put anything you want here! There is just a spacer below for demo purposes! -->
+    <div style="height: 200px;"></div>
+  </section>
+
+  <!-- Content section -->
+  <section class="py-5">
+    <div class="container">
+      <h1>Section Heading</h1>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.</p>
+    </div>
+  </section>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'home',
+
+}
+</script>
